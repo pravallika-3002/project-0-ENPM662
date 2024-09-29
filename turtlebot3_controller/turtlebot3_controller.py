dd8d95abdea1f4e6723116aa599bf5f5413b76aa
pravallika-3002 <lakshmipravallika.adibhatla@gmail.com>
2024-09-29 19:21:01 -0400

    turtlebot3_controller.py

--- turtlebot3_controller/turtlebot3_controller.py
+++ turtlebot3_controller/turtlebot3_controller.py
@@ -14,33 +14,29 @@ class TurtleBot3Controller(Node):
         self.velocity_value = 0.2  #constant velocity 0.2 m/s
 #the amount of time required to reach the target is caluculated
         self.time_to_reach_target = self.x_target / self.velocity_value
-
         self.get_logger().info(f'Moving towards the target distance: {self.x_target} meters at {self.velocity_value} m/s')
         self.get_logger().info(f'Estimated time to reach the target: {self.time_to_reach_target} seconds')
-
         self.move_turtlebot()
 
     def move_turtlebot(self):
 #velocity message is created
-        velocity_msg = Twist()
-        velocity_msg.linear.x = self.velocity_value
-        velocity_msg.angular.z = 0.0  # No rotation
+        velocity = Twist()
+        velocity.linear.x = self.velocity_value
+        velocity.angular.z = 0.0  #no rotational motion
 #start time is obtained
         start_time = time.time()
 #turtlebot is moved for a calculated time
         while time.time() - start_time < self.time_to_reach_target:
-            self.publisher_.publish(velocity_msg)
+            self.publisher_.publish(velocity)
             self.get_logger().info(f'Moving... Time: {time.time() - start_time:.2f} seconds')
             time.sleep(0.1)  #delayed to avoid flooding of topic
 #turtlebot stops after reaching the target
-        velocity_msg.linear.x = 0.0
-        self.publisher_.publish(velocity_msg)
+        velocity.linear.x = 0.0
+        self.publisher_.publish(velocity)
         self.get_logger().info('Reached the target distance. Stopping.')
-
 def main(args=None):
     rclpy.init(args=args)
     node = TurtleBot3Controller()
-
     try:
         rclpy.spin(node)
     except KeyboardInterrupt:
